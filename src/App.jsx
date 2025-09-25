7397f73a6f029e91c8eb8de95886f7447ffdd34a
Dayesoro <dayesoro@gmail.com>
2025-09-25 17:17:46 +0100

    feat(movies): display trending movies and save search data
    - Integrated Appwrite BaaS for backend data storage
    
    - Connected app to database to persist searched movies

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useEffect, useState} from 'react'
-import {useDebounce} from 'react-use'
 import Search from "./components/Search.jsx";
 import Spinner from "./components/Spinner.jsx";
 import MovieCard from "./components/MovieCard.jsx";
+import {useDebounce} from 'react-use'
+import {updateSearchCount} from "./appwrite.js";
 
 const API_BASE_URL = "https://api.themoviedb.org/3";
 
@@ -17,16 +18,17 @@ const API_OPTIONS = {
 }
 
 const App = () => {
+    const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('')
     const [searchTerm, setSearchTerm] = useState('');
 
     const [movieList, setMovieList] = useState([]);
     const [errorMessage, setErrorMessage] = useState('');
     const [isLoading, setIsLoading] = useState(false);
-    const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('')
+
 
     // Debounce the search term to prevent making too many API requests
     // by waiting for the user to stop typing for 500ms
-    useDebounce(() => setDebouncedSearchTerm(searchTerm), 500,[searchTerm]);
+    useDebounce(() => setDebouncedSearchTerm(searchTerm), 1000,[searchTerm]);
 
 
     const fetchMovies = async (query = '' ) => {
@@ -53,6 +55,10 @@ const App = () => {
             }
 
             setMovieList(data.results || []);
+
+            if(query && data.results.length > 0) {
+               await updateSearchCount(query, data.results[0]);
+            }
 
         } catch (error) {
             console.error(`Error fetching movies: ${error}`)
---
+++ src/appwrite.js
@@ -0,0 +1,52 @@
+import {Client, TablesDB, Query, ID} from "appwrite";
+
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const TABLE_ID = import.meta.env.VITE_APPWRITE_TABLE_ID;
+
+
+
+const client = new Client()
+    .setEndpoint('https://fra.cloud.appwrite.io/v1')
+    .setProject(PROJECT_ID);
+
+const tablesDB = new TablesDB(client);
+
+export const updateSearchCount = async (searchTerm, movie) => {
+    // 1. Use Appwrite SDK to check if the search term exists in the database
+    try {
+        const result = await tablesDB.listRows({
+            databaseId: DATABASE_ID,
+            tableId: TABLE_ID,
+            queries: [
+                Query.equal('searchTerm', searchTerm),
+            ]
+        });
+
+        // 2. If it does, update the count
+        if(result.rows.length > 0){
+            const rowData =  result.rows[0];
+
+            await tablesDB.updateRow({
+                databaseId: DATABASE_ID,
+                tableId: TABLE_ID,
+                rowId: rowData.$id,
+                data: {count: rowData.count + 1},
+            });
+            // 3. If it doesn't, create a new document with the search term and count as 1
+        }else {
+            await tablesDB.createRow({
+                databaseId: DATABASE_ID,
+                tableId: TABLE_ID,
+                rowId: ID.unique(),
+                data: {
+                    searchTerm: searchTerm,
+                    count: 1,
+                    poster_url: `https://image.tmdb.org/t/p/w500/${movie.poster_path}`,
+                    movie_id: movie.id},
+            });
+        }
+    } catch (error) {
+        console.error(error);
+    }
+}
